380f2837084264536dfadf6c13b53dbdd4e9e4b3
adonson <adonson@users.noreply.github.com>
2020-02-06 15:53:00 +0900

    restart

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -35,12 +35,14 @@ $(function () {
       var html = buildHTML(data);
       $('.chat-main__message-list').append(html);
       $('form')[0].reset();
-      $('.new_message').prop('disabled', false);
       $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight });
+      $('.form__submit').prop('disabled', false);
     })
     .fail(function () {
       alert("メッセージ送信に失敗しました");
+      $('.form__submit').prop('disabled', false);
     });
+
   })
 
   var reloadMessages = function() {
